2200fba8c324545fcdb67fa9032ac0e557a15659
Parminder Singh <simba23@gmail.com>
2020-12-07 17:45:20 -0800

    update azure-terraform-demo.tf to remove duplicate resource names

--- azure-aks-demo.tf
+++ azure-aks-demo.tf
@@ -1,62 +1,70 @@
 # Azure Demo
 
-resource "tfe_workspace" "azure_terraform_demo_setup_ps" {
-  name           = "azure-terraform-demo-ps"
+resource "tfe_workspace" "azure_aks_demo_setup_ps" {
+  name           = "azure-aks-demo-ps"
   organization   = var.org_name
   queue_all_runs = false
   vcs_repo {
-    identifier     = "simba23/terraform-azure-demo-ps"
+    identifier     = "simba23/terraform-aks-demo-ps"
     branch         = "main"
     oauth_token_id = var.oauth_token_id
   }
 }
 
-resource "tfe_variable" "azure_subscription_tf_id" {
+resource "tfe_variable" "azure_subscription_id" {
   key          = "ARM_SUBSCRIPTION_ID"
   value        = var.azure_subscription_id
   category     = "env"
-  workspace_id = tfe_workspace.azure_terraform_demo_setup_ps.id
+  workspace_id = tfe_workspace.azure_aks_demo_setup_ps.id
   description  = "Azure Subscription ID"
 }
 
-resource "tfe_variable" "azure_client_tf_id" {
+resource "tfe_variable" "azure_client_id" {
   key          = "ARM_CLIENT_ID"
   value        = var.azure_client_id
   category     = "env"
-  workspace_id = tfe_workspace.azure_terraform_demo_setup_ps.id
+  workspace_id = tfe_workspace.azure_aks_demo_setup_ps.id
   description  = "Azure Client ID"
 }
 
-resource "tfe_variable" "azure_tenant_tf_id" {
+resource "tfe_variable" "azure_tenant_id" {
   key          = "ARM_TENANT_ID"
   value        = var.azure_tenant_id
   category     = "env"
-  workspace_id = tfe_workspace.azure_terraform_demo_setup_ps.id
+  workspace_id = tfe_workspace.azure_aks_demo_setup_ps.id
   description  = "Azure Tenant ID"
 }
 
-resource "tfe_variable" "azure_client_secret_tf" {
+resource "tfe_variable" "azure_client_secret" {
   key          = "ARM_CLIENT_SECRET"
   value        = var.azure_client_secret
   category     = "env"
-  workspace_id = tfe_workspace.azure_terraform_demo_setup_ps.id
+  workspace_id = tfe_workspace.azure_aks_demo_setup_ps.id
   description  = "Azure Client Secret"
   sensitive    = true
 }
 
-resource "tfe_variable" "azure_appId_tf" {
-  key          = "appId"
-  value        = var.appId
+resource "tfe_variable" "azure_randomvar" {
+  key          = "randomvar"
+  value        = var.randomvar
   category     = "terraform"
-  workspace_id = tfe_workspace.azure_terraform_demo_setup_ps.id
+  workspace_id = tfe_workspace.azure_aks_demo_setup_ps.id
   description  = "Application ID"
 }
 
-resource "tfe_variable" "azure_password_tf" {
+resource "tfe_variable" "azure_appId" {
+  key          = "appId"
+  value        = var.appId
+  category     = "terraform"
+  workspace_id = tfe_workspace.azure_aks_demo_setup_ps.id
+  description  = "Application ID"
+}
+
+resource "tfe_variable" "azure_password" {
   key          = "password"
   value        = var.password
   category     = "terraform"
-  workspace_id = tfe_workspace.azure_terraform_demo_setup_ps.id
+  workspace_id = tfe_workspace.azure_aks_demo_setup_ps.id
   description  = "Client Password"
   sensitive    = true
 }
